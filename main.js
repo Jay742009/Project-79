f5bcbbd129ab7ccbae1174fc2584fe3e5bd650c1
Jay742009 <72810212+Jay742009@users.noreply.github.com>
2020-12-04 09:36:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function show() {
+    document.getElementById("img_1").style.display=inline-block;
+    document.getElementById("img_2").style.display=inline-block;
+    document.getElementById("p_1").style.display=inline-block;
+    document.getElementById("p_2").style.display=inline-block;
+}
